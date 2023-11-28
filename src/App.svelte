cb2488836f26837424574f02dcb137a5755ba794
SajjadPourali <sajjad@pourali.com>
2023-11-28 18:50:59 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import init, { greet } from "types-wrapper";
+  import Test from "./lib/Test.svelte";
+
+  init().then(() => {
+    console.log("init wasm-pack");
+    // greet("from vite!");
+  });
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<Test />
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
